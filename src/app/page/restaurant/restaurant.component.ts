c90343bff979c569b72d2170c54878819123f044
Himitris <antoine.gautier83@gmail.com>
2024-07-02 22:08:29 +0200

    commit

--- src/app/page/restaurant/restaurant.component.ts
+++ src/app/page/restaurant/restaurant.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-
-interface Dish {
-  name: string;
-  price: number;
-}
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrl: './restaurant.component.scss',
+  styleUrls: ['./restaurant.component.scss'],
 })
 export class RestaurantComponent {
+  @ViewChild('fullscreenOverlay') fullscreenOverlay!: ElementRef;
+
+  openFullscreen(event: MouseEvent) {
+    const clickedImg = event.target as HTMLImageElement;
+    const overlay = document.querySelector(
+      '.fullscreen-overlay'
+    ) as HTMLElement;
+    const fullscreenImg = overlay.querySelector('img') as HTMLImageElement;
+
+    fullscreenImg.src = clickedImg.src;
+    overlay.classList.add('active');
+  }
+
+  closeFullscreen() {
+    const overlay = document.querySelector(
+      '.fullscreen-overlay'
+    ) as HTMLElement;
+    overlay.classList.remove('active');
+  }
 }
