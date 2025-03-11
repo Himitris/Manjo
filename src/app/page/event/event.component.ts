944c1f974454c1efe6e075473442bc2af460241b
Himitris <antoine.gautier83@gmail.com>
2025-03-11 15:47:24 +0100

    oui

--- src/app/page/event/event.component.ts
+++ src/app/page/event/event.component.ts
@@ -7,15 +7,6 @@ import { Component } from '@angular/core';
 })
 export class EventComponent {
   events = [
-    { date: 'Le vendredi 28 juin', description: 'Karaoké soirée' },
-    { date: 'Le vendredi 5 juillet', description: 'Karaoké soirée' },
-    {
-      date: 'Le samedi 27 juillet',
-      description: 'Cours de salsa (débutant à avancé)',
-    },
-    {
-      date: 'Le dimanche 18 août',
-      description: 'Cours de salsa (débutant et avancé)',
-    },
+    { date: 'A venir', description: 'Saison 2025' },
   ];
 }
